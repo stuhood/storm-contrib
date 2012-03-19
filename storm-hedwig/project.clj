28a24a30c982bd5581070ac50e808fd30cb0de12
Stu Hood <stuhood@twitter.com>
2012-03-19 02:14:37 -0700

    ocd

--- storm-hedwig/project.clj
+++ storm-hedwig/project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.apache.bookkeeper/hedwig-client "4.0.0"
                    :exclusions [com.sun.jdmk/jmxtools
                                 com.sun.jmx/jmxri]]
-                [org.apache.bookkeeper/hedwig-protocol "4.0.0"]]
+                 [org.apache.bookkeeper/hedwig-protocol "4.0.0"]]
   :dev-dependencies [[storm "0.7.0"]
                      [org.clojure/clojure "1.2.0"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
